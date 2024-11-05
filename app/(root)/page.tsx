739374512adb1355be2b339edd1a6cce93b0e76a
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-05 17:40:51 +0530

    running 2:52:00

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { Search } from "lucide-react";
 import { title } from "process";
@@ -10,7 +11,7 @@ export default async function Home({searchParams}:{
     searchParams:Promise< {query?: string}>
 }){
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data:posts} = await sanityFetch({query: STARTUPS_QUERY})
   
   return (
     <>
@@ -36,6 +37,7 @@ export default async function Home({searchParams}:{
           )}
         </ul>
    </section>
+   <SanityLive />
     </>
 )
 }
