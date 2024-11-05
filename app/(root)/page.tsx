20e0a8748e44bcf40846bdfb1e787c8615f007b5
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-05 11:37:14 +0530

    running 1:43:00

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <>
     <section className="pink_container">
-   <h1 className="heading">Home</h1>
+   <h1 className="heading">Pitch Your Startup, <br /> Connect With Enterpreneurs</h1>
+   <p className="sub-heading !max-w-3xl">
+    Submit Ideas, Vote on Pitches, and Get Noticed in virtual Competitions.
+   </p>
    </section>
     </>
   );
