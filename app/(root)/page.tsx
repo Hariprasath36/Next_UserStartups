b5325f97453afa5d6fa87a22be27adf070b7e58f
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-05 12:49:33 +0530

    running 2:00:00

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 import { Search } from "lucide-react";
+import { title } from "process";
 
 
 export default async function Home({searchParams}:{
     searchParams:Promise< {query?: string}>
 }){
   const query = (await searchParams).query;
+  const posts = [{_createdAt:'Yesterday',
+    views:55,
+    _id:1,
+    description: "this is ",
+    image:"/logo.png",
+    category:"Robots",
+    title:"We Robots",
+  },
+];
   return (
     <>
     <section className="pink_container">
@@ -15,6 +26,21 @@ export default async function Home({searchParams}:{
    </p>
    <SearchForm query={query}/>
    </section>
+
+   <section className="section_container">
+     <p className="text-30-semibold">{query ? `Search results for "${query}"` : 'All Startups' }
+     </p>
+
+     <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+   </section>
     </>
-  );
+)
 }
